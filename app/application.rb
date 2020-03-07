581a80e8e0920078f4a8668431cf928e0883405d
Sir Eric <echeungsk@gmail.com>
2020-03-07 21:55:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,8 +10,8 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/add/)
-      
+    elsif req.path.match(/cart/)
+      @@cart
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
